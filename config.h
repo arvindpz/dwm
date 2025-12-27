3201b6c5f0e9be14eefbc0254b505f146325b9e4
arvindpz <parvind97@gmail.com>
2025-12-27 11:07:13 +0530

    Add hotkey for browser -> Mod + b

--- config.h
+++ config.h
@@ -45,6 +45,10 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* applications */
+#define TERM "st"
+#define BROWSER "brave-browser"
+
 /* key definitions */
 #define MODKEY Mod4Mask           // Use windows key as modifier
 #define TAGKEYS(KEY,TAG) \
@@ -59,13 +63,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-e", "/usr/bin/zsh", "-l", NULL };
+static const char *termcmd[]  = { TERM, "-e", "/usr/bin/zsh", "-l", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      spawn,          SHCMD(BROWSER) },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
