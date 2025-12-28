055555669b9d07c175fcc68219bf0063e12bb20e
arvindpz <parvind97@gmail.com>
2025-12-28 20:33:44 +0530

    Patch for xrdb

--- config.def.h
+++ config.def.h
@@ -16,15 +16,16 @@ static const int showbar			= 1;		/* 0 means no bar */
 static const int topbar			 = 1;		/* 0 means bottom bar */
 static const char *fonts[]		  = { "monospace:size=10" };
 static const char dmenufont[]	   = "monospace:size=10";
-static const char col_gray1[]	   = "#222222";
-static const char col_gray2[]	   = "#444444";
-static const char col_gray3[]	   = "#bbbbbb";
-static const char col_gray4[]	   = "#eeeeee";
-static const char col_cyan[]		= "#005577";
-static const char *colors[][3]	  = {
-	/*			   fg		 bg		 border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 #include "vanitygaps.c"
@@ -96,7 +97,7 @@ static const Layout layouts[] = {
 #define STOP_MONITOR "xrandr --output DisplayPort-1 --off && xrandr --output edP --primary"
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { TERM, "-e", "/usr/bin/zsh", "-l", NULL };
 
 static const Key keys[] = {
@@ -169,6 +170,7 @@ static const Key keys[] = {
 	{ MODKEY,					   XK_F2,	 saveSession,	{0} },
 	// Restart dwm using F3
 	{ MODKEY|ShiftMask|ControlMask, XK_F3,	 quit,		   {1} },
+	{ MODKEY|ShiftMask|ControlMask, XK_F5,	 xrdb,		   { .v = NULL } },
 
 	/* I don't quit easily */
 	{ MODKEY|ShiftMask|ControlMask, XK_q,	  quit,		   {0} },
