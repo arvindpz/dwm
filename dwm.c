3ce1c208e83cec32ef10a894e407ddbf6b621540
arvindpz <parvind97@gmail.com>
2025-12-28 13:15:21 +0530

    Fullscreen patch

--- dwm.c
+++ dwm.c
@@ -215,6 +215,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscreen();
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -819,7 +820,13 @@ focus(Client *c)
         XSetInputFocus(dpy, root, RevertToPointerRoot, CurrentTime);
         XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
     }
-    selmon->sel = c;
+    if(selmon->sel && selmon->sel->isfullscreen){
+        togglefullscreen();
+        selmon->sel = c;
+        togglefullscreen();
+    } else {
+        selmon->sel = c;
+    }
     drawbars();
 }
 
@@ -1837,6 +1844,14 @@ togglefloating(const Arg *arg)
 }
 
 void
+togglefullscreen()
+{
+    if (selmon->sel){
+        setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
+    }
+}
+
+void
 toggletag(const Arg *arg)
 {
     unsigned int newtags;
@@ -1881,6 +1896,7 @@ unmanage(Client *c, int destroyed)
 {
     Monitor *m = c->mon;
     XWindowChanges wc;
+    int fullscreen = (selmon->sel == c && selmon->sel->isfullscreen)?1:0;
 
     detach(c);
     detachstack(c);
@@ -1898,6 +1914,8 @@ unmanage(Client *c, int destroyed)
     }
     free(c);
     focus(NULL);
+    if (fullscreen)
+        togglefullscreen();
     updateclientlist();
     arrange(m);
 }
