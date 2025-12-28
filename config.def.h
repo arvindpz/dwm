837211a0e0c1580e8cc247c583170b92816b6c60
arvindpz <parvind97@gmail.com>
2025-12-28 19:29:49 +0530

    Patch for vanitygaps + fibonacci

--- config.def.h
+++ config.def.h
@@ -7,6 +7,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;		/* border pixel of windows */
 static const unsigned int snap	  = 32;	   /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar			= 1;		/* 0 means no bar */
 static const int topbar			 = 1;		/* 0 means bottom bar */
 static const char *fonts[]		  = { "monospace:size=10" };
@@ -21,6 +26,8 @@ static const char *colors[][3]	  = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+
+#include "vanitygaps.c"
 
 /* applications */
 #define TERM "st"
@@ -52,8 +59,10 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 static const Layout layouts[] = {
 	/* symbol	 arrange function */
 	{ "[]=",	  tile },	/* first entry is default */
-	{ "><>",	  NULL },	/* no layout function means floating behavior */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",	  monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
