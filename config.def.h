13deb225b2f32d3e9af3857a139015eb93411c54
arvindpz <parvind97@gmail.com>
2026-04-28 19:59:43 +0530

    Add default tags for obs and kdenlive

--- config.def.h
+++ config.def.h
@@ -79,9 +79,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class          instance        title                                       tags mask         isfloating        monitor */
-	{ "Gimp",         NULL,	          NULL,	                                         0,                1,               -1 },
-	{ NULL,           NULL,	          "meet.google.com is sharing your screen.",     1 << 8,           1,               -1 },
+	/* class	instance        title						tags mask	isfloating	monitor */
+	{ "Gimp",	NULL,		NULL,						0,		1,		-1 },
+	{ "obs",	"obs",		NULL,						1 << 1,		0,		-1 },
+	{ "kdenlive",	"kdenlive",	NULL,						1 << 1,		0,		-1 },
+	{ NULL,		NULL,		"meet.google.com is sharing your screen.",	1 << 8,		1,		-1 },
 };
 
 /* layout(s) */
